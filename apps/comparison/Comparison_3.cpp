1d112959cdea6a105a5041f583473fd0d34528b8
tautcony <tautcony@gmail.com>
2016-08-25 20:43:59 +0800

    Update cpp version.

--- apps/comparison/Comparison_3.cpp
+++ apps/comparison/Comparison_3.cpp
@@ -1,14 +1,18 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<ctype.h>
 
 int n,m,i,j,k,a[100];
-char url[100][300];
-char cover[300];
+char url[100][300],cover[300],*p;
 
 int cmpfunc(const void * a, const void * b)
 {
-    return strcmp((const char *)a, (const char *)b);
+    const char *x = (const char *)a, *y = (const char *)b;
+    int p = strlen(x), q = strlen(y);
+    if (p<q) return -1;
+    if (p>q) return 1;
+    return strcmp(x,y);
 }
 
 int main()
@@ -21,12 +25,18 @@ int main()
         if (url[n-1][strlen(url[n-1])-2]=='p'){
             memcpy(cover,url[n-1],300*sizeof(char));
             n--;
+        } else {
+            i = strlen(url[n-1]);
+            if (isdigit(url[n-1][i-5]))
+                url[n-1][i-4]=0;
+            else
+                url[n-1][i-5]=0;
         }
     n--;
     qsort(url, n, 300*sizeof(char), cmpfunc);
-    if (n % 3!=0) return 0;
     if (cover[0]) printf("%s\n\n",cover);
     printf("Comparison (right click on the image and open it in a new tab to see the full-size one)\n");
     printf("Source________________________________________________Encode\n\n");
-    for (i=0;i<n;i+=3) printf("[URL=%s][IMG]%s[/IMG][/URL] [URL=%s][IMG]%s[/IMG][/URL]\n",url[i],url[i+1],url[i+2],url[i+1]);   
+    for (i=0;i<n;i+=3) printf("[URL=%s.png][IMG]%ss.png[/IMG][/URL] [URL=%sv.png][IMG]%ss.png[/IMG][/URL]\n",url[i],url[i],url[i],url[i]);
+    return 0;
 }

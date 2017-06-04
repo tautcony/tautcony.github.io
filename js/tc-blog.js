39f3df7cfd3947667db9bcaa439a3734fadef451
tautcony <tautcony@gmail.com>
2017-06-04 17:53:28 +0800

    Improve some code.

--- js/tc-blog.js
+++ js/tc-blog.js
@@ -7,7 +7,7 @@
  /*!
  * Hux Blog v1.6.0 (http://startbootstrap.com)
  * Copyright 2016 @huxpro
- * Licensed under Apache 2.0 
+ * Licensed under Apache 2.0
  */
 
 // Tooltip Init
@@ -18,23 +18,23 @@
 
 
 // make all images responsive
-/* 
+/*
  * Unuse by Hux
  * actually only Portfolio-Pages can't use it and only post-img need it.
  * so I modify the _layout/post and CSS to make post-img responsive!
  */
 // $(function() {
-//  $("img").addClass("img-responsive");
+//    $("img").addClass("img-responsive");
 // });
 
 // responsive tables
-$(document).ready(function () {
+$(document).ready(function() {
     $("table").wrap("<div class='table-responsive'></div>");
     $("table").addClass("table");
 });
 
 // responsive embed videos
-$(document).ready(function () {
+$(document).ready(function() {
     $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
     $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
     $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
@@ -49,66 +49,63 @@ jQuery(document).ready(function ($) {
     if ($(window).width() > MQL) {
         var headerHeight = $('.navbar-custom').height(),
             bannerHeight = $('.intro-header .container').height();
-        $(window).on('scroll', { previousTop: 0 },
-            function () {
-                var currentTop = $(window).scrollTop(),
-                    $catalog = $('.side-catalog');
+        $(window).scroll({ previousTop: 0 }, function (event) {
+            var currentTop = $(window).scrollTop();
+            var $catalog = $('.side-catalog');
 
-                //check if user is scrolling up by mouse or keyborad
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-visible');
-                    } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
-                    }
+            //check if user is scrolling up by mouse or keyborad
+            if (currentTop < event.data.previousTop) {
+                //if scrolling up...
+                if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                    $('.navbar-custom').addClass('is-visible');
                 } else {
-                    //if scrolling down...
-                    $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                    $('.navbar-custom').removeClass('is-visible is-fixed');
                 }
-                this.previousTop = currentTop;
+            } else {
+                //if scrolling down...
+                $('.navbar-custom').removeClass('is-visible');
+                if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+            }
+            event.data.previousTop = currentTop;
 
-
-                //adjust the appearance of side-catalog
-                $catalog.show()
-                if (currentTop > bannerHeight) {
-                    $catalog.addClass('fixed')
-                } else {
-                    $catalog.removeClass('fixed')
-                }
-            });
+            //adjust the appearance of side-catalog
+            $catalog.show()
+            if (currentTop > bannerHeight) {
+                $catalog.addClass('fixed')
+            } else {
+                $catalog.removeClass('fixed')
+            }
+        });
     }
 });
 
 function async(url, func) {
-    var script = document.createElement('script'),
-        orgins = document.getElementsByTagName('script')[0];
+    var script = document.createElement('script');
+    var orgins = document.getElementsByTagName('script')[0];
     script.src = url;
     if (func) { script.addEventListener('load', function (e) { func(null, e); }, false); }
     orgins.parentNode.insertBefore(script, orgins);
 }
 
-async("http://cdn.bootcss.com/fastclick/1.0.6/fastclick.min.js", function () {
-    // var $nav = document.querySelector("nav");
-    // if($nav) FastClick.attach($nav);
+async("http://cdn.bootcss.com/fastclick/1.0.6/fastclick.min.js", function() {
+    var $nav = document.querySelector("nav");
+    if($nav) FastClick.attach($nav);
+});
 
-    // global FastClick!!
-    FastClick.attach(document.body);
-})
+jQuery(document).ready(function ($) {
+    // only load tagcloud.js in tag.html
+    if ($('#tag_cloud').length !== 0) {
+        async("/js/jquery.tagcloud.js", function() {
+            $.fn.tagcloud.defaults = {
+                //size: {start: 1, end: 1, unit: 'em'},
+                color: { start: '#bbbbee', end: '#0085a1' },
+            };
+            $('#tag_cloud a').tagcloud();
+        });
+    }
+});
 
-// only load tagcloud.js in tag.html
-if ($('#tag_cloud').length !== 0) {
-    async("/js/jquery.tagcloud.js", function () {
-        $.fn.tagcloud.defaults = {
-            //size: {start: 1, end: 1, unit: 'em'},
-            color: { start: '#bbbbee', end: '#0085a1' },
-        };
-        $('#tag_cloud a').tagcloud();
-    });
-}
-
-$(function () {
+jQuery(document).ready(function ($) {
     $("#gotop").click(function () {
         $("html, body").animate({ scrollTop: 0 }, 1000);
     });
@@ -116,15 +113,16 @@ $(function () {
         if ($(this).scrollTop() > 300) {
             $('#gotop').fadeIn("fast");
         } else {
-            $('#gotop').stop().fadeOut("fast");
+            $('#gotop').stop().fadeOut("fast"); 
         }
     });
 });
 
-$(function () {
+jQuery(document).ready(function ($) {
     var initalTitle = document.title;
-    var gogatsubyou = ["_(:3 」∠)_", "_(・ω・｣∠)_", "_(:з)∠)_", "_(┐「ε:)_", "_(:3」∠❀", "_(:зゝ∠)_", "_(:3」[＿]", "_(:3」[＿]", "ヾ(:3ﾉｼヾ)ﾉｼ"];
-    document.addEventListener('visibilitychange', function (event) {
+    var gogatsubyou = ["_(:3 」∠)_", "_(・ω・｣∠)_", "_(:з)∠)_"   , "_(┐「ε:)_" , "_(:3」∠❀",
+                       "_(:зゝ∠)_" , "_(:3」[＿]" , "ヾ(:3ﾉｼヾ)ﾉｼ", "(¦3ꇤ[▓▓]", "_( -ω-` )⌒)_"];
+    document.addEventListener('visibilitychange', function(event) {
         if (!document.hidden) {
             document.title = initalTitle;
         } else {

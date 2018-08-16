f12982203cfa6d1d31826aebe5d1c985aee359bf
TautCony <tautcony@gmail.com>
2018-08-16 21:34:54 +0800

    [Improve] use Vue.js more correctly.

--- js/tcupdate.js
+++ js/tcupdate.js
@@ -1,32 +1,48 @@
-var url1 = "'http://'+site+'/bin/'+name+'.v'+info.version+'.exe'";
-var url2 = "'http://'+site+'/'+url+'/'+name+'.v'+info.version+'.7z'";
-
-if (typeof(Vue) !== 'undefined') {
-    Vue.component('download-link', {
-        props: ['name', 'info', 'site', 'width'],
-        template: '<a :class="width" :href="'+url1+'" target="_blank" rel="noopener"><icon class="icon-cloud-download"></icon>{{name}}<br>version {{(info.version)}}<span>Updated {{info.date}}.</span></a>'
+if (typeof(Vue) !== "undefined") {
+    Vue.component("download-link", {
+        props: ["name", "info", "site", "width"],
+        template:   `<a :class="width" :href="href" target="_blank" rel="noopener">
+                        <div class="icon icon-cloud-download"></div>
+                        {{name}}<br>version {{(info.version)}}
+                        <span>Updated {{info.date}}.</span>
+                    </a>`,
+        computed: {
+            href() {
+                return `http://${this.site}/bin/${this.name}.v${this.info.version}.exe`;
+            }
+        }
     });
-    Vue.component('history-download-link', {
-        props: ['name', 'url', 'info', 'site'],
-        template: '<li><icon class="icon-cloud-download"></icon><a class="link" :href="'+url2+'" target="_blank" rel="noopener"> {{name}} {{info.version}} </a>({{info.date}})</il>'
+    Vue.component("history-download-link", {
+        props: ["name", "url", "info", "site"],
+        template:   `<li>
+                        <div class="icon icon-cloud-download"></div>
+                        <a class="link" :href="href" target="_blank" rel="noopener">
+                            {{name}} {{info.version}}
+                        </a>
+                        ({{info.date}})
+                    </li>`,
+        computed: {
+            href() {
+                return `http://${this.site}/${this.url}/${this.name}.v${this.info.version}.7z`;
+            }
+        }
     });
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open("GET", "/json/tcupdate.json", true);
-    xhr.onload = function() {
+    xhr.onload = () => {
         if (xhr.readyState === 4 && xhr.getResponseHeader("content-type").indexOf("application/json") !== -1) {
-           var releases_data = JSON.parse(xhr.responseText);
-            var tcupdate = new Vue({
-                el: '#tool-downloads',
+            window.tcupdate = new Vue({
+                el: "#tool-downloads",
                 data: {
-                    site: '7xsjmh.com2.z0.glb.clouddn.com',
-                    data: releases_data
+                    site: "7xsjmh.com2.z0.glb.clouddn.com",
+                    data: JSON.parse(xhr.responseText)
                 }
             });
         } else {
             console.error(xhr);
         }
     };
-    xhr.onerror = function() {
+    xhr.onerror = () => {
         console.error(xhr.statusText);
     };
     xhr.send();

90fc7850a1d48a05b2d2adab7fd41ea6e1241883
tautcony <tautcony@gmail.com>
2018-10-08 21:41:14 +0800

    [Fix] new tcupdate with github api.

--- js/tcupdate.js
+++ js/tcupdate.js
@@ -1,49 +1,102 @@
 if (typeof(Vue) !== "undefined") {
-    Vue.component("download-link", {
-        props: ["name", "info", "site", "width"],
-        template:   `<a :class="width" :href="href" target="_blank" rel="noopener">
-                        <div class="icon icon-cloud-download"></div>
-                        {{name}}<br>version {{(info.version)}}
-                        <span>Updated {{info.date}}.</span>
-                    </a>`,
-        computed: {
-            href() {
-                return `http://${this.site}/bin/${this.name}.v${this.info.version}.exe`;
+    Vue.filter("time2date", (time_string) => {
+        const date = new Date(time_string);
+        if (Number.isNaN(date.getTime())) {
+            return time_string;
+        }
+        return date.toDateString();
+    });
+    Vue.filter("info2url", (info) => {
+        const assets = info.assets;
+        if (assets.length === 0) {
+            return info.zipball_url;
+        }
+        for (const asset of assets) {
+            if (asset.name.endsWith(".exe")) {
+                return asset.browser_download_url;
             }
+        }
+        return assets[0].browser_download_url;
+    });
+    Vue.component("download-link", {
+        props: ["owner", "repo", "width"],
+        template:   `<a :class="width" :href="info | info2url" target="_blank" rel="noopener">
+                        <div class="icon icon-cloud-download"></div>
+                        {{this.repo.replace(/-/g, '')}}
+                        <br>
+                        version {{this.info.tag_name}}
+                        <span>Updated {{this.info.published_at | time2date}}.</span>
+                    </a>`,
+        data() {
+            return {
+                info: {
+                    assets: [{browser_download_url: "#", name: "Loading..."}],
+                    name: "Loading...",
+                    tag_name: "Loading...",
+                    published_at: "Loading...",
+                }
+            };
+        },
+        created() {
+            this.$http.get(`https://api.github.com/repos/${this.owner}/${this.repo}/releases/latest`).then(response => {
+                this.info = response.data;
+                console.log(response);
+            }).catch(exception => {
+                console.error(`[${exception.status}] Failed to load from '${this.owner}/${this.repo}/latest'`);
+            });
         }
     });
     Vue.component("history-download-link", {
-        props: ["name", "url", "info", "site"],
+        props: ["name", "published_at", "info"],
         template:   `<li>
                         <div class="icon icon-cloud-download"></div>
-                        <a class="link" :href="href" target="_blank" rel="noopener">
-                            {{name}} {{info.version}}
+                        <a class="link" :href="browser_download_url" target="_blank" rel="noopener">
+                            {{name}}
                         </a>
-                        ({{info.date}})
+                        ({{published_at}})
                     </li>`,
         computed: {
-            href() {
-                return `http://${this.site}/${this.url}/${this.name}.v${this.info.version}.7z`;
+            browser_download_url() {
+                const assets = this.info.assets;
+                if (assets.length === 0) {
+                    return this.info.zipball_url;
+                }
+                return assets[0].browser_download_url
             }
         }
     });
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "/json/tcupdate.json", true);
-    xhr.onload = () => {
-        if (xhr.readyState === 4 && xhr.getResponseHeader("content-type").indexOf("application/json") !== -1) {
-            window.tcupdate = new Vue({
-                el: "#tool-downloads",
-                data: {
-                    site: "7xsjmh.com2.z0.glb.clouddn.com",
-                    data: JSON.parse(xhr.responseText)
-                }
+    Vue.component("history-download", {
+        props: ["owner", "repo"],
+        template: `
+        <ul id="all-releases_ct">
+            <history-download-link v-for="info in (this.all_release)"
+                :key="info.node_id"
+                :name="info.name"
+                :published_at="info.published_at | time2date"
+                :info="info"
+            />
+        </ul>`,
+        data() {
+            return {
+                all_release: [
+                    {
+                        assets: [{browser_download_url: "#", name: "Loading..."}],
+                        node_id: "",
+                        name: "Loading...",
+                        published_at: "Loading..."
+                    }
+                ]
+            };
+        },
+        created() {
+            this.$http.get(`https://api.github.com/repos/${this.owner}/${this.repo}/releases`).then(response => {
+                this.all_release = response.data;
+            }).catch(exception => {
+                console.error(`[${exception.status}] Failed to load from '${this.owner}/${this.repo}'`);
             });
-        } else {
-            console.error(xhr);
         }
-    };
-    xhr.onerror = () => {
-        console.error(xhr.statusText);
-    };
-    xhr.send();
+    });
+    window.tcupdate = new Vue({
+        el: "#tool-downloads"
+    });
 }

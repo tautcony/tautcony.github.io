feeb9891c29edc8aade2c1ef63b6faf4a95e8140
TautCony <tautcony@gmail.com>
2020-02-24 21:16:43 +0800

    feat: compress js/css as gzip.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,7 +1,15 @@
 const merge = require("webpack-merge");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 const baseWebpackConfig = require("./webpack.base.conf");
 
 module.exports = merge(baseWebpackConfig, {
     mode: "production",
+    plugins: [
+        new CompressionPlugin({
+            test: [/\.js(\?.*)?$/i, /\.css(\?.*)?$/i],
+            include: ["js", "css"],
+            cache: true,
+        }),
+    ]
 });
